166bcc9dff420800349f7b7eb827ea5f181868e8
monukumar98 <monu012as@gmail.com>
2024-08-13 21:14:49 +0530

    Variables

---
+++ Lec1/Arithmetic_Operators.java
@@ -0,0 +1,22 @@
+package Lec1;
+
+public class Arithmetic_Operators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 8;
+		int b = 3;
+		int c = a + b;
+		System.out.println(c);
+		c = a - b;
+		System.out.println(c);
+		int d = a % b;
+		System.out.println(d);
+		// Assignment Operators
+		int x = 5;
+		x += 5;// x = x+ 5;
+		x = +5;// x= 5
+
+	}
+
+}
---
+++ Lec1/Test.java
@@ -0,0 +1,14 @@
+package Lec1;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Cdoing Blcoks");
+		System.out.print("Hello");
+		System.out.println("Bye");
+		System.out.println("Okay");
+
+	}
+
+}
