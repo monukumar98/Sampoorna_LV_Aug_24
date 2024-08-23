d67ffa61877b3ed6bc96897b12676feb398e0124
monukumar98 <monu012as@gmail.com>
2024-08-23 22:10:25 +0530

    Fibo GCD  Pascal Triangle

---
+++ Lec4/continue_Demo.java
@@ -0,0 +1,17 @@
+package Lec4;
+
+public class continue_Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for (int i = 1; i <= 5; i++) {
+			if (i == 2) {
+				continue;
+			}
+			System.out.println(i);
+		}
+
+	}
+
+}
