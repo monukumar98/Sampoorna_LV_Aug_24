2bd6d1b4ac825fb6da47c5dcb876f68c179a88d5
monukumar98 <monu012as@gmail.com>
2024-10-21 22:03:59 +0530

    Recursion and Baecktracking-1

---
+++ Lec22/Queen_Permutation.java
@@ -0,0 +1,28 @@
+package Lec22;
+
+public class Queen_Permutation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 4;
+		int tq = 2;
+
+		boolean[] board = new boolean[n];
+		Permutation(board, tq, "", 0);
+	}
+
+	public static void Permutation(boolean[] board, int tq, String ans, int qpsf) {
+		if (qpsf == tq) {
+			System.out.println(ans);
+			return;
+		}
+		for (int i = 0; i < board.length; i++) {
+			if (board[i] == false) {
+				board[i] = true;// queen placed
+				Permutation(board, tq, ans + "b" + i + "q" + qpsf, qpsf + 1);
+				board[i] = false;// UNDO
+			}
+		}
+
+	}
+}
