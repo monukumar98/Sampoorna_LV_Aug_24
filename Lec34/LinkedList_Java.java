138012c9e9be0af27f49ec196c67c50219c68324
monukumar98 <monu012as@gmail.com>
2024-11-22 22:27:09 +0530

    LInkedList Qps

--- Lec34/LinkedList_Java.java
+++ Lec34/LinkedList_Java.java
@@ -8,7 +8,7 @@ public class LinkedList_Java {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		LinkedList<Integer> q = new LinkedList<>();
-		q.add(10);
+		q.add(10);// add last
 		q.add(20);
 		q.add(30);
 		q.add(40);
