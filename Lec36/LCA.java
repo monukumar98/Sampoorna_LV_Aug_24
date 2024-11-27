cd628ce357b2bd8c1f5dec467cc7be40d917ae04
monukumar98 <monu012as@gmail.com>
2024-11-27 10:14:05 +0530

    View Mirror LCA Path Sum

--- Lec36/LCA.java
+++ Lec36/LCA.java
@@ -1,9 +1,35 @@
 package Lec36;
 
 public class LCA {
+	public class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		TreeNode(int x) {
+			val = x;
+		}
+	}
+
+	class Solution {
+		public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+			if (root == null) {
+				return null;
+			}
+			if (root == p || root == q) {
+				return root;
+			}
+			TreeNode left = lowestCommonAncestor(root.left, p, q);
+			TreeNode right = lowestCommonAncestor(root.right, p, q);
+			if (left != null && right != null) {
+				return root;
+			} else if (left == null) {
+				return right;
+			} else {
+				return left;
+			}
+
+		}
 
 	}
 
